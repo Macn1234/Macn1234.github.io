9a8a3f218085b318978f2a702a795f2bcf96d68d
Miguel Angel Cortes Nivia <macortesn@unal.edu.co>
2017-08-24 18:53:56 -0500

    Modificacion de index para hacer una prueba del background

--- js/App.js
+++ js/App.js
@@ -11,38 +11,38 @@ if (!window.location.origin) {
 }
 
 $(document).ready(function() {
-	
+
 	// Change all href links that go out of the origin to open in a new window
 	// Relative links that start with /, #, or . (ie /post/my-post or ../post/my-post or #test)
 	// should be untouched
 	$("a").each(function(i, el) {
 		var href = $(el).attr("href");
-		
+
 		if (typeof href === "string" && href.indexOf(window.location.origin) === -1 && "/.#".indexOf(href[0]) === -1) {
 			$(el).attr("target", "_blank");
 		}
 	});
-	
+
 	// Wrap pygments tables in table-responsive classes
 	// Credit to GitHub user dazinator
 	// https://github.com/alexurquhart/hugo-geo/issues/17
 	$( ".highlighttable" ).wrap("<div class='table-responsive'></div>");
-	
+
 	// Highlight all on load if highlighting
 	if (typeof hljs !== "undefined") {
 		hljs.initHighlightingOnLoad();
 	}
-	
+
 	// If d3 is not loaded do not show the globe
 	if (typeof d3 === "undefined") {
 		return;
 	}
-	
+
 	// Peace out if in a mobile browser
 	if ($.browser.mobile) {
-		return;	
+		return;
 	}
-	
+
 	// If you are hosting your site on say GitHub pages at the URL
 	// "person.github.com/repo-name", loading the world.json file will
 	// fail unless the script knows the base url it's calling from
@@ -52,50 +52,50 @@ $(document).ready(function() {
 	if (typeof window.baseURL === "undefined") {
 		window.baseURL = window.location.origin;
 	}
-	
+
 	// Load world json file
 	d3.json(window.baseURL + "/js/world.json", function(error, world) {
 		if (error) {
 			return;
 		}
-		
+
 		var Start = Date.now();
 		var width = $('#menu').outerWidth(true);
 	   	var height = $('#menu').outerHeight(true);
 		var rotate = [10];
 	    var velocity = [0.001];
-	
+
 		var projection = d3.geo.orthographic()
 			.scale(1000)
 			.translate([250, height])
 			.rotate([-80])
 			.clipAngle(90)
 			.precision(0.1);
-	
+
 		var path = d3.geo.path()
 			.projection(projection);
-	
+
 		var graticule = d3.geo.graticule();
-	
+
 		var svg = d3.select("#menu").insert("svg", "#menu-content")
 			.attr("id", "menu-globe")
 			.attr("width", width)
 			.attr("height", height);
-	
+
 		svg.append("defs").append("path")
 			.datum({type: "Sphere"})
 			.attr("id", "sphere")
 			.attr("d", path);
-	
+
 		svg.append("use")
 			.attr("class", "stroke")
 			.attr("xlink:href", "#sphere");
-	
+
 		svg.append("path")
 			.datum(graticule)
 			.attr("class", "graticule")
 			.attr("d", path);
-		
+
 		svg.insert("path", ".graticule")
 			.datum(topojson.feature(world, world.objects.land))
 			.attr("class", "land")
@@ -105,7 +105,7 @@ $(document).ready(function() {
 			.datum(topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; }))
 			.attr("class", "boundary")
 			.attr("d", path);
-		
+
 		var feature = svg.selectAll("path");
 
 		d3.timer(function() {
@@ -113,13 +113,200 @@ $(document).ready(function() {
 			projection.rotate([rotate[0] + velocity[0] * dt]);
 			feature.attr("d", path);
 		});
-		
+
 		function updateWindow(){
 			var w = $('#menu').outerWidth(true);
 			var h = $('#menu').outerHeight(true);
-	
+
 			svg.attr("width", w).attr("height", h);
 		}
 		window.onresize = updateWindow;
 	});
 });
+
+
+(function() {
+
+    var width, height, largeHeader, canvas, ctx, points, target, animateHeader = true;
+
+    // Main
+    initHeader();
+    initAnimation();
+    addListeners();
+
+    function initHeader() {
+        width = window.innerWidth;
+        height = window.innerHeight;
+        target = {x: width/2, y: height/2};
+
+        largeHeader = document.getElementById('large-header');
+        largeHeader.style.height = height+'px';
+
+        canvas = document.getElementById('demo-canvas');
+        canvas.width = width;
+        canvas.height = height;
+        ctx = canvas.getContext('2d');
+
+        // create points
+        points = [];
+        for(var x = 0; x < width; x = x + width/20) {
+            for(var y = 0; y < height; y = y + height/20) {
+                var px = x + Math.random()*width/20;
+                var py = y + Math.random()*height/20;
+                var p = {x: px, originX: px, y: py, originY: py };
+                points.push(p);
+            }
+        }
+
+        // for each point find the 5 closest points
+        for(var i = 0; i < points.length; i++) {
+            var closest = [];
+            var p1 = points[i];
+            for(var j = 0; j < points.length; j++) {
+                var p2 = points[j]
+                if(!(p1 == p2)) {
+                    var placed = false;
+                    for(var k = 0; k < 5; k++) {
+                        if(!placed) {
+                            if(closest[k] == undefined) {
+                                closest[k] = p2;
+                                placed = true;
+                            }
+                        }
+                    }
+
+                    for(var k = 0; k < 5; k++) {
+                        if(!placed) {
+                            if(getDistance(p1, p2) < getDistance(p1, closest[k])) {
+                                closest[k] = p2;
+                                placed = true;
+                            }
+                        }
+                    }
+                }
+            }
+            p1.closest = closest;
+        }
+
+        // assign a circle to each point
+        for(var i in points) {
+            var c = new Circle(points[i], 2+Math.random()*2, 'rgba(255,255,255,0.3)');
+            points[i].circle = c;
+        }
+    }
+
+    // Event handling
+    function addListeners() {
+        if(!('ontouchstart' in window)) {
+            window.addEventListener('mousemove', mouseMove);
+        }
+        window.addEventListener('scroll', scrollCheck);
+        window.addEventListener('resize', resize);
+    }
+
+    function mouseMove(e) {
+        var posx = posy = 0;
+        if (e.pageX || e.pageY) {
+            posx = e.pageX;
+            posy = e.pageY;
+        }
+        else if (e.clientX || e.clientY)    {
+            posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+            posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+        }
+        target.x = posx;
+        target.y = posy;
+    }
+
+    function scrollCheck() {
+        if(document.body.scrollTop > height) animateHeader = false;
+        else animateHeader = true;
+    }
+
+    function resize() {
+        width = window.innerWidth;
+        height = window.innerHeight;
+        largeHeader.style.height = height+'px';
+        canvas.width = width;
+        canvas.height = height;
+    }
+
+    // animation
+    function initAnimation() {
+        animate();
+        for(var i in points) {
+            shiftPoint(points[i]);
+        }
+    }
+
+    function animate() {
+        if(animateHeader) {
+            ctx.clearRect(0,0,width,height);
+            for(var i in points) {
+                // detect points in range
+                if(Math.abs(getDistance(target, points[i])) < 4000) {
+                    points[i].active = 0.3;
+                    points[i].circle.active = 0.6;
+                } else if(Math.abs(getDistance(target, points[i])) < 20000) {
+                    points[i].active = 0.1;
+                    points[i].circle.active = 0.3;
+                } else if(Math.abs(getDistance(target, points[i])) < 40000) {
+                    points[i].active = 0.02;
+                    points[i].circle.active = 0.1;
+                } else {
+                    points[i].active = 0;
+                    points[i].circle.active = 0;
+                }
+
+                drawLines(points[i]);
+                points[i].circle.draw();
+            }
+        }
+        requestAnimationFrame(animate);
+    }
+
+    function shiftPoint(p) {
+        TweenLite.to(p, 1+1*Math.random(), {x:p.originX-50+Math.random()*100,
+            y: p.originY-50+Math.random()*100, ease:Circ.easeInOut,
+            onComplete: function() {
+                shiftPoint(p);
+            }});
+    }
+
+    // Canvas manipulation
+    function drawLines(p) {
+        if(!p.active) return;
+        for(var i in p.closest) {
+            ctx.beginPath();
+            ctx.moveTo(p.x, p.y);
+            ctx.lineTo(p.closest[i].x, p.closest[i].y);
+            ctx.strokeStyle = 'rgba(156,217,249,'+ p.active+')';
+            ctx.stroke();
+        }
+    }
+
+    function Circle(pos,rad,color) {
+        var _this = this;
+
+        // constructor
+        (function() {
+            _this.pos = pos || null;
+            _this.radius = rad || null;
+            _this.color = color || null;
+        })();
+
+        this.draw = function() {
+            if(!_this.active) return;
+            ctx.beginPath();
+            ctx.arc(_this.pos.x, _this.pos.y, _this.radius, 0, 2 * Math.PI, false);
+            ctx.fillStyle = 'rgba(156,217,249,'+ _this.active+')';
+            ctx.fill();
+        };
+    }
+
+    // Util
+    function getDistance(p1, p2) {
+        return Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2);
+    }
+
+})();
